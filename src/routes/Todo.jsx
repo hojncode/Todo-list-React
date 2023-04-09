760bb88bf91685b881b2aeecbf32313b23e1f3c1
hojncode <glicm12@gmail.com>
2023-04-09 15:00:15 +0900

    Add: todo 페이지 - 체크박스 추가

--- src/routes/Todo.jsx
+++ src/routes/Todo.jsx
@@ -1,44 +1,56 @@
 import { useState } from "react";
 
 function Todo() {
-    const initTodo = [
-      1,2
-    ]
+  const initTodo = [
+    { text: 1, completed: false },
+    { text: 2, completed: true },
+  ];
 
-    const [todo,setTodo] = useState(initTodo)
-    const [inputTodo, setInputTodo] = useState("")
+  const [todo, setTodo] = useState(initTodo);
+  const [inputTodo, setInputTodo] = useState("");
 
+  const handleInputChange = (e) => {
+    // e.preventDefault()
+    const { value } = e.target;
+    setInputTodo(value);
+  };
 
-    const handleInputChange = (e) => {
-        e.preventDefault()
-        const { value } = e.target;
-        setInputTodo(value)
-    }
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setTodo([...todo, { text: inputTodo, completed: false }]);
+    setInputTodo("");
+  };
 
-    const handleFormSubmit = (e) => {
-        e.preventDefault()
-        setTodo([...todo, inputTodo])
-        setInputTodo("")
-    }
+  const handleChecked = (e) => {
+    const checkedTodo = [...todo];
+    checkedTodo[e].completed = !checkedTodo[e].completed;
+    setTodo(checkedTodo);
+    console.log(todo[e].completed);
+  };
+
   return (
     <>
       <div>Todo</div>
       <div>
         <form onSubmit={handleFormSubmit}>
-          <input value={inputTodo} onChange={handleInputChange}/>
-          <button>TODO추가</button>
+          <input data-testid="new-todo-input"  type="text" value={inputTodo} onChange={handleInputChange} />
+          <button data-testid="new-todo-add-button">TODO추가</button>
         </form>
       </div>
-          {todo.map((td,index) => (
-            <li key={index}><label>
-                <input type="checkbox" />
-                  <span>{td}</span>
-              </label>
-              <button data-testid="modify-button">수정</button>
-              <button data-testid="delete-button">삭제</button>
-              </li>
-          ))}
-         
+      {todo.map((td, _i) => (
+        <li key={_i}>
+          <label>
+            <input
+              type="checkbox"
+              checked={td.completed}
+              onChange={() => handleChecked(_i)}
+            />
+            <span>{td.text}</span>
+          </label>
+          <button data-testid="modify-button">수정</button>
+          <button data-testid="delete-button">삭제</button>
+        </li>
+      ))}
     </>
   );
 }
