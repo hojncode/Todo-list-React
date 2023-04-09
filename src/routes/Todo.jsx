e8d6fc3b695b5e2f95f5e3de49db49fd785679ae
hojncode <glicm12@gmail.com>
2023-04-09 23:28:38 +0900

    Add: todo 수정하기

--- src/routes/Todo.jsx
+++ src/routes/Todo.jsx
@@ -2,47 +2,69 @@ import { useState } from "react";
 
 function Todo() {
   const initTodo = [
-    { text: 100, completed: false },
-    { text: 200, completed: true },
+    { id: 0, todo: "todo1", isCompleted: false, isEdit: false },
+    { id: 1, todo: "todo2", isCompleted: true, isEdit: false },
   ];
 
   const [todo, setTodo] = useState(initTodo);
-    const [inputTodo, setInputTodo] = useState("");
-   
+  const [inputTodo, setInputTodo] = useState("");
+  const [editTodo, setEditTodo] = useState("");
+  //   const [newEdit, setNewEdit] = useState(todo.todo || ""); //! 이 방법은 사용하지 말것.
+
   const handleInputChange = (e) => {
-    // e.preventDefault()
     const { value } = e.target;
     setInputTodo(value);
   };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    setTodo([...todo, { text: inputTodo, completed: false ,isEdit: false}]);
+    setTodo([
+      ...todo,
+      {
+        id: todo.length,
+        todo: inputTodo,
+        isCompleted: false,
+        isEdit: false,
+        userId: 0,
+      },
+    ]);
     setInputTodo("");
+    console.log("생성", todo);
   };
 
   const handleChecked = (e) => {
     const checkedTodo = [...todo];
-    checkedTodo[e].completed = !checkedTodo[e].completed;
+    checkedTodo[e].isCompleted = !checkedTodo[e].isCompleted;
     setTodo(checkedTodo);
-    console.log(todo[e].completed);
   };
 
   const handleDelete = (e) => {
     const deletedTodo = [...todo];
     deletedTodo.splice(e, 1); //해당 TODO를 splice() 메서드를 사용하여 todos 배열에서 제거 (2번째 인자에 1을 추가하여 현재 index만 삭제-여기서는 e로 index를 가져옴)
     setTodo(deletedTodo);
-    console.log(todo)
   };
 
-    const handleEditInput = (e) => {
-        const editedTodo = [...todo]
-        editedTodo[e].isEdit = !editedTodo[e].isEdit
-        setTodo(editedTodo)
-      console.log(todo)
-  }
+  const handleOpenEdit = (e) => {
+    const editedTodo = [...todo];
+    editedTodo[e].isEdit = !editedTodo[e].isEdit;
+    setTodo(editedTodo);
+    setEditTodo(editedTodo[e].todo);
+  };
 
-    
+  const handleEditInputChange = (e) => {
+    const { value } = e.target;
+    console.log("value", value);
+    setEditTodo(value);
+  };
+
+  const handleEditTextSubmit = (id) => {
+    const newTodo = [...todo];
+    newTodo[id].todo = editTodo;
+    newTodo[id].isEdit = false;
+    setTodo(newTodo);
+    setEditTodo("");
+  };
+
   return (
     <>
       <div>Todo</div>
@@ -57,25 +79,58 @@ function Todo() {
           <button data-testid="new-todo-add-button">TODO추가</button>
         </form>
       </div>
-      {todo.map((td, _i) => (
-        <li key={_i}>
-              {}
-          <label>
-            <input
-              type="checkbox"
-              checked={td.completed}
-              onChange={() => handleChecked(_i)}
-            />
-            <span>{td.text}</span>
-          </label>
-              {td.isEdit ? <>
-                  <input data-testid="modify-input" />
-                  <button data-testid="submit-button">제출</button>
-                  <button data-testid="cancel-button" onClick={()=> handleEditInput(_i)}>취소</button>
-              </> : <button data-testid="modify-button" onClick={()=> handleEditInput(_i)}>수정</button>}
-          <button data-testid="delete-button" onClick={() => handleDelete(_i)}>삭제</button>
-        </li>
-      ))}
+      {todo.map((td) => {
+        return (
+          <li key={td.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={td.isCompleted}
+                onChange={() => handleChecked(td.id)}
+              />
+            </label>
+            {td.isEdit ? (
+              <>
+                <input
+                  data-testid="modify-input"
+                  type="text"
+                  value={editTodo}
+                  onChange={handleEditInputChange}
+                />
+                <button
+                  onClick={() => handleEditTextSubmit(td.id)}
+                  data-testid="submit-button"
+                >
+                  제출
+                </button>
+
+                <button
+                  data-testid="cancel-button"
+                  onClick={() => handleOpenEdit(td.id)}
+                >
+                  취소
+                </button>
+              </>
+            ) : (
+              <>
+                <span>{td.todo}</span>
+                <button
+                  data-testid="modify-button"
+                  onClick={() => handleOpenEdit(td.id)}
+                >
+                  수정
+                </button>
+              </>
+            )}
+            <button
+              data-testid="delete-button"
+              onClick={() => handleDelete(td.id)}
+            >
+              삭제
+            </button>
+          </li>
+        );
+      })}
     </>
   );
 }
