36517df2dea50d63bee811127b82d3612d588a9b
hojncode <glicm12@gmail.com>
2023-04-09 13:49:05 +0900

    Add: 회원가입페이지 - API post

--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,10 +1,16 @@
+import axios from "axios";
 import { useState } from "react";
+import { Redirect, useHistory } from "react-router";
+
 
 function Signup() {
+  const API = "https://www.pre-onboarding-selection-task.shop";
+  const history = useHistory();
   const [disable, setDisable] = useState(true);
   const [inputEmail, setInputEmail] = useState("");
   const [inputPw, setInputPw] = useState("");
-  
+  const userdata = { email: inputEmail, password: inputPw };
+
   const handleEmail = (e) => {
     e.preventDefault();
     const { value } = e.target;
@@ -17,16 +23,28 @@ function Signup() {
     setInputPw(value);
   };
 
-  const handleValid = (e) => {
+  const handleValid = async (e) => {
     e.preventDefault();
     if (inputEmail.includes("@") && inputPw.length >= 7) {
       setDisable(false);
     }
-
+    try {
+      const response = await axios.post(`${API}/auth/signup`, userdata, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      alert("통과");
+      console.log(response.data);
+      return <Redirect to="/signin"/>
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <div className="App">
+      Signup
       <form className="Form" onChange={handleValid}>
         <input
           data-testid="email-input"
