8483fb29a6027461a7bbf75db36ec0d987b4539a
hojncode <glicm12@gmail.com>
2023-04-10 22:29:34 +0900

    Add: useSearchParams

--- src/routes/User.jsx
+++ src/routes/User.jsx
@@ -1,10 +1,21 @@
-import { Link, Outlet, useOutletContext, useParams } from "react-router-dom";
+import {
+  Link,
+  Outlet,
+  useOutletContext,
+  useParams,
+  useSearchParams,
+} from "react-router-dom";
 import { users } from "../db";
 
 function User() {
   const { userIdGet } = useParams();
   console.log(userIdGet);
   console.log(useOutletContext());
+  // const [readSearchParams, setSearchParams] = useSearchParams();
+  // console.log(readSearchParams.get("users"));
+  // setTimeout(() => {
+  //   setSearchParams({ users: "haha" });
+  // },1000);
   return (
     <div>
       <h1>userssss{userIdGet}</h1>
