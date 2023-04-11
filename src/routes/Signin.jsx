fd0b41abe2c56bb3e213696fdfd8086414fb4411
hojncode <glicm12@gmail.com>
2023-04-11 23:23:37 +0900

    Add: 로그인/회원가입 - 리다이렉션&유효성검사 완료

--- src/routes/Signin.jsx
+++ src/routes/Signin.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
-import { useState } from "react";
-import { useHistory } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function Signin() {
   const API = "https://www.pre-onboarding-selection-task.shop";
@@ -8,6 +8,10 @@ function Signin() {
   const [inputEmail, setInputEmail] = useState("");
   const [inputPw, setInputPw] = useState("");
   const userdata = { email: inputEmail, password: inputPw };
+  const access_token = localStorage.getItem("JWTtoken");
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
   const handleEmail = (e) => {
     e.preventDefault();
     const { value } = e.target;
@@ -31,7 +35,7 @@ function Signin() {
           "Content-Type": "application/json",
         },
       });
-      alert("통과", response);
+      // alert("통과", response);
       console.log(response.data);
       localStorage.setItem("JWTtoken", JSON.stringify(response.data));
     } catch (error) {
@@ -39,36 +43,53 @@ function Signin() {
     }
   };
 
+  useEffect(() => {
+    if (access_token !== null) {
+      setLoading(true);
+      setTimeout(() => {
+        navigate("/todo");
+        setLoading(false);
+      }, 1000);
+    }
+  }, []);
+
   return (
     <div className="App">
-      Signin
-      <form className="Form" onChange={handleValid}>
-        <input
-          data-testid="email-input"
-          placeholder="email"
-          required
-          type="email"
-          name="email"
-          onChange={handleEmail}
-          value={inputEmail}
-        />
-        <input
-          data-testid="password-input"
-          placeholder="password"
-          required
-          type="password"
-          minLength="8"
-          onChange={handlePW}
-          value={inputPw}
-        />
-        <button
-          data-testid="signin-button"
-          disabled={disable}
-          // onClick={checkValid}
-        >
-          로그인
-        </button>
-      </form>
+      {loading ? (
+        "로그인 체크... Todo 리스트로 이동합니다"
+      ) : (
+        <>
+          Signin
+          <form className="Form" onChange={handleValid}>
+            <input
+              data-testid="email-input"
+              placeholder="email"
+              required
+              type="email"
+              name="email1"
+              onChange={handleEmail}
+              value={inputEmail}
+            />
+            <input
+              data-testid="password-input"
+              placeholder="password"
+              required
+              type="password"
+              // name="password"
+              minLength="8"
+              onChange={handlePW}
+              value={inputPw}
+            />
+            <button
+              data-testid="signin-button"
+              disabled={disable}
+              // onClick={checkValid}
+            >
+              로그인
+            </button>
+          </form>
+        </>
+      )}
     </div>
   );
 }
--- src/routes/Signup.jsx
+++ src/routes/Signup.jsx
@@ -1,15 +1,20 @@
 import axios from "axios";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Redirect, useHistory } from "react-router";
-
+import { useNavigate } from "react-router-dom";
 
 function Signup() {
   const API = "https://www.pre-onboarding-selection-task.shop";
-  const history = useHistory();
   const [disable, setDisable] = useState(true);
   const [inputEmail, setInputEmail] = useState("");
   const [inputPw, setInputPw] = useState("");
   const userdata = { email: inputEmail, password: inputPw };
+  const access_token = localStorage.getItem("JWTtoken");
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(
+    "이메일 주소와 패스워드를 입력해 주세요.(가입 성공시 로그인 페이지로 이동합니다.)"
+  );
 
   const handleEmail = (e) => {
     e.preventDefault();
@@ -23,7 +28,7 @@ function Signup() {
     setInputPw(value);
   };
 
-  const handleValid = async (e) => {
+  const handleValidSumbit = async (e) => {
     e.preventDefault();
     if (inputEmail.includes("@") && inputPw.length >= 7) {
       setDisable(false);
@@ -35,43 +40,75 @@ function Signup() {
         },
       });
       alert("통과");
-      console.log(response.data);
-      return <Redirect to="/signin"/>
+      console.log(response.status);
+      console.log(response.data.message);
     } catch (error) {
-      console.error(error);
+      console.error(error.response.data.message);
+      if (error.response.data.message.includes("이미")) {
+        setErrorMsg(error.response.data.message);
+      } else if (
+        error.response.data.message.includes("email must contain a @ string")
+      ) {
+        setErrorMsg("이메일 주소와 패스워드를 입력해 주세요.");
+      } else if (
+        error.response.data.message.includes(
+          "password must be longer than or equal to 8 characters"
+        ) ||
+        inputEmail.includes(".com" || ".net")
+      ) {
+        setErrorMsg("패스워드는 8자리 이상입니다.");
+      }
     }
   };
 
+  useEffect(() => {
+    if (access_token !== null) {
+      setLoading(true);
+      setTimeout(() => {
+        navigate("/signin");
+        setLoading(false);
+      }, 1000);
+    }
+  }, []);
+
   return (
     <div className="App">
-      Signup
-      <form className="Form" onChange={handleValid}>
-        <input
-          data-testid="email-input"
-          placeholder="email"
-          required
-          type="email"
-          name="email"
-          onChange={handleEmail}
-          value={inputEmail}
-        />
-        <input
-          data-testid="password-input"
-          placeholder="password"
-          required
-          type="password"
-          minLength="8"
-          onChange={handlePW}
-          value={inputPw}
-        />
-        <button
-          data-testid="signup-button"
-          disabled={disable}
-          // onClick={checkValid}
-        >
-          회원가입
-        </button>
-      </form>
+      {loading ? (
+        "로그인 확인..."
+      ) : (
+        <>
+          <span>Signup</span>
+
+          <form className="Form" onChange={handleValidSumbit}>
+            <input
+              data-testid="email-input"
+              placeholder="email"
+              required
+              type="email"
+              name="email"
+              onChange={handleEmail}
+              value={inputEmail}
+            />
+            <input
+              data-testid="password-input"
+              placeholder="password"
+              required
+              type="password"
+              minLength="8"
+              onChange={handlePW}
+              value={inputPw}
+            />
+            <button
+              data-testid="signup-button"
+              disabled={disable}
+              // onClick={checkValid}
+            >
+              회원가입
+            </button>
+            <span>{errorMsg}</span>
+          </form>
+        </>
+      )}
     </div>
   );
 }
